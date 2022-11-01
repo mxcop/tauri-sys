ad78d9422055c80e377e1951756b7dde7be51349
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-11-01 17:41:39 +0100

    Revert "fix: use cmd on for build on windows"
    This reverts commit 8490f545500264b6d61d8219598498879f378d03.

--- build.rs
+++ build.rs
@@ -1,35 +1,17 @@
 use std::process::Command;
 
 fn main() {
-    if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(&[
-                "/C",
-                "esbuild",
-                "--outdir=dist",
-                "--format=esm",
-                "--bundle",
-                "tauri/tooling/api/src/app.ts",
-                "tauri/tooling/api/src/clipboard.ts",
-                "tauri/tooling/api/src/tauri.ts",
-                "tauri/tooling/api/src/event.ts",
-                "tauri/tooling/api/src/mocks.ts",
-            ])
-            .output()
-            .unwrap();
-    } else {
-        Command::new("esbuild")
-            .args(&[
-                "--outdir=dist",
-                "--format=esm",
-                "--bundle",
-                "tauri/tooling/api/src/app.ts",
-                "tauri/tooling/api/src/clipboard.ts",
-                "tauri/tooling/api/src/tauri.ts",
-                "tauri/tooling/api/src/event.ts",
-                "tauri/tooling/api/src/mocks.ts",
-            ])
-            .output()
-            .unwrap();
-    };
+    Command::new("esbuild")
+        .args([
+            "--outdir=dist",
+            "--format=esm",
+            "--bundle",
+            "tauri/tooling/api/src/app.ts",
+            "tauri/tooling/api/src/clipboard.ts",
+            "tauri/tooling/api/src/tauri.ts",
+            "tauri/tooling/api/src/event.ts",
+            "tauri/tooling/api/src/mocks.ts",
+        ])
+        .output()
+        .unwrap();
 }
