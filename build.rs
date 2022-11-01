8490f545500264b6d61d8219598498879f378d03
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-11-01 17:27:39 +0100

    fix: use cmd on for build on windows

--- build.rs
+++ build.rs
@@ -1,17 +1,35 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("esbuild")
-        .args([
-            "--outdir=dist",
-            "--format=esm",
-            "--bundle",
-            "tauri/tooling/api/src/app.ts",
-            "tauri/tooling/api/src/clipboard.ts",
-            "tauri/tooling/api/src/tauri.ts",
-            "tauri/tooling/api/src/event.ts",
-            "tauri/tooling/api/src/mocks.ts",
-        ])
-        .output()
-        .unwrap();
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&[
+                "/C",
+                "esbuild",
+                "--outdir=dist",
+                "--format=esm",
+                "--bundle",
+                "tauri/tooling/api/src/app.ts",
+                "tauri/tooling/api/src/clipboard.ts",
+                "tauri/tooling/api/src/tauri.ts",
+                "tauri/tooling/api/src/event.ts",
+                "tauri/tooling/api/src/mocks.ts",
+            ])
+            .output()
+            .unwrap();
+    } else {
+        Command::new("esbuild")
+            .args(&[
+                "--outdir=dist",
+                "--format=esm",
+                "--bundle",
+                "tauri/tooling/api/src/app.ts",
+                "tauri/tooling/api/src/clipboard.ts",
+                "tauri/tooling/api/src/tauri.ts",
+                "tauri/tooling/api/src/event.ts",
+                "tauri/tooling/api/src/mocks.ts",
+            ])
+            .output()
+            .unwrap();
+    };
 }
